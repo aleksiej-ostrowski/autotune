05af54495d19aca607cc21548f62013de9ed4cba
Aleksiej Ostrowski <aleksiej.ostrowski@gmail.com>
2022-05-28 23:58:37 +0300

    new version

--- README.md
+++ README.md
@@ -1,101 +1,39 @@
-# xgboost-AutoTune
-Package allows for auto-tuninng `xgbooxt.XGBRegressor` parameters. Model usues GridSearchCV. Tested for Python 3.5.2.
+This experimental version for auto-tuning is based on script by Sylwia Mielnicka https://github.com/SylwiaOliwia2/xgboost-AutoTune 
 
-## Preface
-**Please keep in mind it's not tutorial on boosting methods**, but library for auto-tuning them. I assume you are familiar with bosting. If not, firstly visit the pages below: 
-
-http://xgboost.readthedocs.io/en/latest/model.html 
-
-https://machinelearningmastery.com/gentle-introduction-xgboost-applied-machine-learning/  
-
-## Instalation
-`pip3 install git+git://github.com/SylwiaOliwia2/xgboost-AutoTune@choose-params`
-
-Note that xgboost-AutoTune depends on Numpy and Sklearn.
+# AutoTune for regressors
+Package allows for auto-tuning `XGBRegressor`, `GradientBoostingRegressor` and `LGBMRegressor` parameters. Model usues `GridSearchCV`. Tested for Python 3.8.10
 
 ## Create scorer
-Before runnheader_titleing you need to define scoring. The easier way is using `sklearn.metrics.make_scorer`. You can see example in [custom_metrics.py](https://github.com/SylwiaOliwia2/xgboost-AutoTune/blob/choose-params/custom_metrics.py).
+You need to define scoring. The easier way is using `sklearn.metrics.make_scorer`. 
 
 ## Fast run
-To run it type in your code:
 
 ```
-from xgboost_autotune import fit_parameters
-from sklearn.metrics import make_scorer, accuracy_score
+import xgboost
+from sklearn.metrics import make_scorer, mean_squared_error
+from autotune import fit_parameters
 
-accuracy = make_scorer(accuracy_score, greater_is_better=True)
-fitted_model = fit_parameters(initial_model = xgboost.XGBRegressor(), initial_params_dict = {}, X_train = X, y_train = y_train, min_loss = 0.01, scoring=accuracy, n_folds=5)
+accuracy = make_scorer(mean_squared_error, greater_is_better=False)
+fitted_model = fit_parameters(model = xgboost.XGBRegressor(), X_train = X, y_train = Y, scoring=accuracy, n_folds=2)    
+
+Y2_pred = fitted_model.predict(X2)
 ```
 
 ### Parameters:
-* **initial_model** - `xgboost.XGBRegressor` model. You can leave parameters empty:
 
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`model = xgboost.XGBRegressor()` or predefine them (they will be used, until better params will be chosen):
+* **X_train** - pandas DataFrame, input variables.
+* **y_train** - pandas series, value for prediction.
+* **scoring** - used to evaluate the best model.
+* **n_folds** - number of folds used in GridSearchCV.
 
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`model = xgboost.XGBRegressor(max_depth = 7, learning_rate = 0.1,  colsample_bytree=0.7)`
-* **initial_params_dict** - dictionary of values to be tested. If empty (as in the example)- domain parameters' arrays will be 
-used for search. If you will overwrite domain parameters, provide arrays of values in the dictionary, ex:
 
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`{'susample': [0.6, 0.8, 1], 'max_depth': [3,5,7]'}`.
+## Test results
 
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;If you type only one value in parameter array then this particular value will be used, without tuning this parameter, ex:
-
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`{'susample': [0.8], 'max_depth': [3,5,7]'}` will use `subsample` = 0.8 and wouldn't look for better `subsample` value.
-
-* **X_train** - pandas DataFrame, input variables
-* **y_train** - pandas series, labels for prediction
-* **min_loss** - minimum scoring loss required for further parameter research (in neighbourhood of the best value).
-* **scoring** - used to evalueate the best model. See **Create scorer** section above.
-* **n_folds** - number of folds used in GridSearchCV
-
-## Implementation details
-### General note
-Full GridSearch is time- and memory-demanding, so xgboost-AutoTune tunes parameters in the following steps (one by one, from the most robust to the less): 
-1. n_estimators
-2. max_depth, min_child_weight
-3. Gamma                       
-4. n_estimators
-5. Subsample, colsample_bytree
-6. reg_alpha, reg_lambda 
-7. n_estimators and learning_rate 
-
-Some of them are related only to `xgboost`, `LightGBM` or `GBM`. Algorithm picks parameters valid for given model and skip the rest. 
-
-Model is updated by newly chosen parameters in each step. 
-
-### Detailed notes
-Algorithm make GridsearchCV for each in seven steps (see **General note** section) and choose the best value. It uses domian values: 
-```
-{'n_estimators': [30, 50, 70, 100, 150, 200, 300]},                                       
-{'max_depth': [3, 5, 7, 9], 'min_child_weight': [0.001, 0.1, 1, 5, 10, 20], 'min_samples_split': [1,2,5,10,20,30], 'num_leaves': [15, 35, 50, 75, 100,150]}, 
-{'gamma': [0.0, 0.1, 0.2, 0.3, 0.4, 0.5], 'min_samples_leaf': [1,2,5,10,20,30], 'min_child_samples': [2,7,15,25,45], 'min_split_gain': [0, 0.001, 0.1, 1,5, 20]}, 
-{'n_estimators': [30, 50, 70, 100, 150, 200, 300],  'max_features': range(10,25,3)}, 
-{'subsample': [i/10 for i in range(4,10)], 'colsample_bytree': [i/10 for i in range(4,10)], 'feature_fraction': [i/10 for i in range(4,10)]}, 
-{'reg_alpha':[1e-5, 1e-2, 0.1, 1, 25, 100], 'reg_lambda':[1e-5, 1e-2, 0.1, 1, 25, 100]}
-```
-Unless user will provide his own dictionary of values in **initial_params_dict**. 
-
-In each iteration, if chosing the best value from array has improved **scoring** by **min_loss**, algorithm continue searching. It creates new array from the best value, and 2 values in the neighbourhood: 
-
-* If the best value in the previous array had neighbours, then new neighbours will be average between best value and it's previous neighbours. Example: if the best value from `n_estimators`: `[30, 50, 70, 100, 150, 200, 300]` will be 70, than the new array to search will be `[60, 70, 85]`. 
-
-* If the best value is the lowest from the array, it's new value will be `2*best_value- following_value` unless it's bigger then minimal (otherwise minimal posible value). 
-
-* The the best value was the biggest in the array, it will be treated in the similar way, as the lowest one. 
-
-If new values are float and int is required, values are rounded. 
-
-`n_estimators` and `learning_rate` are chosen pairwise. Algorithm takes its values from model and train them pairwise: (n* `n_estimators` , `learning_rate`/ n ). 
-
-## Sources
-xgboost-AutoTune bases on the advises from the posts below: 
-
-http://xgboost.readthedocs.io/en/latest/how_to/param_tuning.html 
-
-https://www.analyticsvidhya.com/blog/2016/03/complete-guide-parameter-tuning-xgboost-with-codes-python/ 
-
-https://machinelearningmastery.com/tune-number-size-decision-trees-xgboost-python/ 
-
-https://www.kaggle.com/prasunmishra/parameter-tuning-for-xgboost-sklearn/notebook 
-
-https://cambridgespark.com/content/tutorials/hyperparameter-tuning-in-xgboost/index.html 
+| Regressor | RMSE |
+| --- | --- |
+| bare **XGBRegressor** | 0.00051 |
+| autotuned **XGBRegressor** | 0.00022 |
+| bare **GradientBoostingRegressor** | 0.00066 |
+| autotuned **GradientBoostingRegressor** | 0.00017 |
+| bare **LGBMRegressor** | 0.00033 |
+| autotuned **LGBMRegressor** | 0.00015 |
---
+++ autotune.py
@@ -0,0 +1,270 @@
+# ------------------------------ #
+#                                #
+#  version 0.0.1                 #
+#                                #
+#  Aleksiej Ostrowski, 2022      #
+#                                #
+#  https://aleksiej.com          #
+#                                #
+# ------------------------------ #
+
+"""
+
+Based on script by Sylwia Mielnicka
+https://github.com/SylwiaOliwia2/xgboost-AutoTune
+
+"""
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
+import numpy as np
+import math
+
+
+def fit_parameters(
+    model,
+    X_train,
+    y_train,
+    scoring,
+    n_folds=5,
+):
+
+    params = set(model.get_params().keys())
+
+    regressor = model.__class__.__name__
+
+    if regressor not in [
+        "XGBRegressor",
+        "GradientBoostingRegressor",
+        "LGBMRegressor"
+    ]:
+        print(f"Maybe your {regressor} doesn't support")
+
+    """
+
+    XGBRegressor:
+
+    'objective', 'base_score', 'booster', 'colsample_bylevel',
+    'colsample_bynode', 'colsample_bytree', 'gamma', 'gpu_id', 'importance_type',
+    'interaction_constraints', 'learning_rate', 'max_delta_step', 'max_depth',
+    'min_child_weight', 'missing', 'monotone_constraints', 'n_estimators',
+    'n_jobs', 'num_parallel_tree', 'random_state', 'reg_alpha', 'reg_lambda',
+    'scale_pos_weight', 'subsample', 'tree_method', 'validate_parameters',
+    'verbosity'
+
+    GradientBoostingRegressor:
+
+    'max_features', 'alpha', 'loss', 'n_estimators', 'min_samples_split', 'init', 
+    'max_leaf_nodes', 'criterion', 'n_iter_no_change', 'validation_fraction',
+    'min_impurity_decrease', 'warm_start', 'tol', 'subsample', 'max_depth',
+    'learning_rate', 'min_weight_fraction_leaf', 'random_state', 'min_samples_leaf',
+    'ccp_alpha', 'min_impurity_split', 'verbose'
+
+    LGBMRegressor:
+
+    'reg_alpha', 'subsample', 'num_leaves', 'silent', 'min_child_samples',
+    'objective', 'n_estimators', 'min_split_gain', 'reg_lambda', 'importance_type',
+    'min_child_weight', 'n_jobs', 'subsample_freq', 'class_weight', 'learning_rate',
+    'max_depth', 'colsample_bytree', 'subsample_for_bin', 'boosting_type',
+    'random_state'
+
+    """
+
+    domain_params_dicts = [
+        {
+            "n_estimators": [100, 150, 200, 300],
+            "learning_rate": [0.0001, 0.001, 0.005, 0.01, 0.04, 0.07, 0.1, 0.2, 0.3]
+        },
+        {
+            "max_depth": [3, 5, 7, 9],
+            "min_child_weight": [0.001, 0.1, 1, 5, 10, 20]
+        },
+        {
+            "gamma": [0.0, 0.5, 1.0, 3.0, 5.0, 10.0, 20.0, 40.0],
+            "colsample_bynode": [0.0, 0.05, 0.1, 0.3, 0.5, 0.7, 1.0],
+            "min_weight_fraction_leaf": [0.0, 0.01, 0.1, 0.3, 0.5],
+            "min_impurity_decrease": [0, 10, 50, 100, 300, 1000],
+            "min_child_samples" : [5, 10, 20, 50, 100, 150],
+            "min_split_gain": [0.0, 0.01, 0.05]
+        },
+        {
+            "max_delta_step": [0, 5, 30, 100, 300, 500],
+            "min_child_weight": [0.001, 0.1, 1, 5, 10, 20],
+            "min_samples_split": [0.0, 0.01, 0.1, 0.3, 0.5, 0.7, 1.0],
+            "min_samples_leaf": [0.0, 0.01, 0.1, 0.3, 0.5, 0.7, 1.0],
+            "num_leaves" : [15, 30, 70, 100, 150, 200, 300]
+        },
+        {
+            "subsample": [0.01, 0.1, 0.3, 0.5, 0.7, 1.0],
+            "colsample_bytree": [0.0, 0.01, 0.1, 0.3, 0.5, 0.7, 1.0],
+            "max_leaf_nodes": [2, 10, 50, 100, 300, 1000]
+        },
+        {
+            "reg_alpha": [1e-5, 1e-2, 0.1, 1, 25, 100],
+            "reg_lambda": [1e-5, 1e-2, 0.1, 1, 25, 100],
+            "alpha": [0.0, 0.01, 0.1, 0.3, 0.5, 0.7, 1.0],
+            "ccp_alpha": [0, 10, 50, 100, 300, 1000]
+        },
+    ]
+
+    best = {}
+    for params_dict in domain_params_dicts:
+
+        allowed = {k: v for k, v in params_dict.items() if k in params}
+
+        if len(allowed) == 0:
+            continue
+
+        print("GridSearch for ", allowed)
+
+        clf = GridSearchCV(
+            model, allowed, scoring=scoring, verbose=0, cv=n_folds, refit=True
+        )
+
+        clf.fit(X_train, y_train)
+
+        new_score = scoring._score_func(clf.predict(X_train), y_train)
+
+        print(f"Regressor {regressor}, score: {new_score}")
+
+        # print(clf.best_score_)
+        # print(clf.best_estimator_)
+        # print(sorted(clf.cv_results_))
+        # print(clf.best_params_)
+
+        best.update(clf.best_params_)
+
+    print(best)
+
+    model.set_params(**best)
+
+    model.fit(X_train, y_train)
+
+    print(f"Regressor {regressor}, the best: {model.get_params()}")
+
+    return model
+
+
+if __name__ == "__main__":
+
+    import xgboost
+    import lightgbm
+    # from catboost import CatBoostRegressor
+    from sklearn.ensemble import GradientBoostingRegressor
+    from sklearn.metrics import make_scorer, mean_squared_error
+
+    import pandas as pd
+    import numpy as np
+
+    train_ = pd.read_csv(
+        "./dataset/train_parody.csv", sep=",", header=None, na_values=["NULL"]
+    )
+    pred_ = pd.read_csv(
+        "./dataset/test_parody.csv", sep=",", header=None, na_values=["NULL"]
+    )
+
+    X, Y = np.ascontiguousarray(train_.iloc[:, :-1]), np.ascontiguousarray(
+        train_.iloc[:, -1]
+    )
+    X2, Y2 = np.ascontiguousarray(pred_.iloc[:, :-1]), np.ascontiguousarray(
+        pred_.iloc[:, -1]
+    )
+
+    table = """| Regressor | RMSE |\n| --- | --- |\n"""
+
+    table += '| bare **XGBRegressor** |'
+
+    model = xgboost.XGBRegressor()
+    model.fit(X, Y, eval_metric='rmse')
+
+    Y2_pred = model.predict(X2)
+    RMSE = mean_squared_error(Y2_pred, Y2)
+
+    table += ' %.5f |\n' % RMSE
+    table += '| autotuned **XGBRegressor** |'
+
+    accuracy = make_scorer(mean_squared_error, greater_is_better=False)
+    fitted_model = fit_parameters(model = xgboost.XGBRegressor(), X_train = X, y_train = Y, scoring=accuracy, n_folds=2)    
+
+    Y2_pred = fitted_model.predict(X2)
+    RMSE = mean_squared_error(Y2_pred, Y2)
+
+    table += ' %.5f |\n' % RMSE
+
+
+    table += "| bare **GradientBoostingRegressor** |"
+
+    model = GradientBoostingRegressor()
+    model.fit(X, Y)  # , eval_metric='rmse')
+
+    Y2_pred = model.predict(X2)
+    RMSE = mean_squared_error(Y2_pred, Y2)
+
+    table += " %.5f |\n" % RMSE
+    table += "| autotuned **GradientBoostingRegressor** |"
+
+    accuracy = make_scorer(mean_squared_error, greater_is_better=False)
+    fitted_model = fit_parameters(
+        model=GradientBoostingRegressor(),
+        X_train=X,
+        y_train=Y,
+        scoring=accuracy,
+        n_folds=2,
+    )
+
+    Y2_pred = fitted_model.predict(X2)
+    RMSE = mean_squared_error(Y2_pred, Y2)
+
+    table += " %.5f |\n" % RMSE
+
+
+    table += "| bare **LGBMRegressor** |"
+
+    model = lightgbm.LGBMRegressor()
+    model.fit(X, Y)  # , eval_metric='rmse')
+
+    Y2_pred = model.predict(X2)
+    RMSE = mean_squared_error(Y2_pred, Y2)
+
+    table += " %.5f |\n" % RMSE
+    table += "| autotuned **LGBMRegressor** |"
+
+    accuracy = make_scorer(mean_squared_error, greater_is_better=False)
+    fitted_model = fit_parameters(
+        model=lightgbm.LGBMRegressor(),
+        X_train=X,
+        y_train=Y,
+        scoring=accuracy,
+        n_folds=2,
+    )
+
+    Y2_pred = fitted_model.predict(X2)
+    RMSE = mean_squared_error(Y2_pred, Y2)
+
+    table += " %.5f |\n" % RMSE
+
+    """
+    table += "| bare **CatBoostRegressor** |"
+
+    model = CatBoostRegressor(verbose=0)
+    model.fit(X, Y)  # , eval_metric='rmse')
+
+    Y2_pred = model.predict(X2)
+    RMSE = mean_squared_error(Y2_pred, Y2)
+
+    table += " %.5f |\n" % RMSE
+    table += "| autotuned **CatBoostRegressor** |"
+
+    accuracy = make_scorer(mean_squared_error, greater_is_better=False)
+    fitted_model = fit_parameters(
+        model=CatBoostRegressor(), X_train=X, y_train=Y, scoring=accuracy, n_folds=2
+    )
+
+    Y2_pred = fitted_model.predict(X2)
+    RMSE = mean_squared_error(Y2_pred, Y2)
+
+    table += " %.5f |\n" % RMSE
+    """
+
+    with open("result_table.txt", "w") as out:
+        out.write(table)
